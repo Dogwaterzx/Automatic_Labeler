1484837a67d9013ccbd4a873643c2519041b1fb2
Dogwaterzx <100448941+Dogwaterzx@users.noreply.github.com>
2022-10-02 11:46:22 -0400

    Addon

---
+++ src/game_template.py
@@ -0,0 +1,11 @@
+# NOTE: This is just a template, made by me!  You can code a game using this template, or basically do anything you like!
+
+
+images = ['']  # Can put anything inside the single quotes.  Copy image address in order to work
+image = images[0]
+
+show_image(image)
+
+labels = get_api_labels(image)
+
+print(labels)
